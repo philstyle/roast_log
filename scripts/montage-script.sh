6f6370ef02bcfec37267737c69592e2f9b55a5e6
Drew Ozier <drew.ozier@gmail.com>
2018-10-24 19:32:02 -0700

    montage notes

--- scripts/montage-script.sh
+++ scripts/montage-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for i in {000..017}; do FOLDER=`printf "%03d\n" $i`; montage -mode Concatenate -tile x1 ${FOLDER}/roastimage* ${FOLDER}.png; done
+#for i in {000..020}; do FOLDER=`printf "%03d\n" $i`; montage -mode Concatenate -tile x1 ${FOLDER}/roastimage* ${FOLDER}.png; done
+#montage -mode Concatenate -tile x1 *.png outs/chpac-7-v29short40.png
+#CLEANUP rm -f *.png
+#CLEANUP for i in {000..020}; do FOLDER=`printf "%03d\n" $i`; rm -f ${FOLDER}/roastim*; done
 
 
-
-#montage -mode Concatenate -tile x1 *.png outs/eadc-8.png
